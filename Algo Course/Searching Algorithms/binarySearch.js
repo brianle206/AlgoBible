f748bc39cdd22df4808ead5b002a80f4504129de
Brian Le <v-brianl@microsoft.com>
2019-04-24 11:22:35 -0700

    adding more algos

---
+++ Algo Course/Searching Algorithms/binarySearch.js
@@ -0,0 +1,8 @@
+/*
+Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.
+
+*/
+
+function binarySearch(arr, val){
+
+}
